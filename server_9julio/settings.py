a456e312d0d1ef28d3cdea4140cd29328b58920f
Lucas Affre <lucasaffre@gmail.com>
2020-11-04 11:11:54 -0300

    LocalDB

--- server_9julio/settings.py
+++ server_9julio/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'server_9julio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '9Julio_DB',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
