bf151538643bc090780b6daafcaf1220a1c57c7d
Lucas Affre <lucasaffre@gmail.com>
2020-11-06 09:45:35 -0300

    Comunicación Dispositivos Finales

--- usuario/views.py
+++ usuario/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Persona, Entrada 
+from django.http import HttpResponse, JsonResponse
+from .models import Persona, Entrada
+
 
 # Create your views here.
 def respuesta(request):
@@ -8,15 +9,38 @@ def respuesta(request):
         nrSocio = request.GET.get('nrSocio', '')
         lugar = request.GET.get('lugar', '')
         print(lugar,nrSocio)
-        #try:  
-        user = Persona.objects.get(nrSocio = nrSocio)
-        print(user)
-        if(lugar == '1'): 
-            lugar = 'general'
-            entrada = Entrada(lugar = 'general',persona = user)
-            entrada.save()
-        rta = '1'     
-        #except:
-            #rta = '-1'
+        try:  
+            user = Persona.objects.get(nrSocio = nrSocio)
+            if(lugar == '1'): 
+                print(user)
+                lugar = 'general'
+                entrada = Entrada(lugar = 'general',persona = user)
+                entrada.save()
+                print(user.general)
+                if(user.general == True):
+                    rta = '1'
+                else:
+                    rta = '0'
+            elif(lugar == '2'):
+                lugar == 'tenis'
+                entrada = Entrada(lugar = 'tenis', persona = user)
+                entrada.save()
+                if(user.tenis == True):
+                    rta = '1'
+                else:
+                    rta = '0'
+            elif(lugar == '3'):
+                lugar = 'piletas'
+                entrada = Entrada(lugar = 'piletas', persona = user)
+                entrada.save()
+                if(user.pileta == True):
+                    rta = '1'
+                else:
+                    rta = '0'
+            else:
+                rta = '0'
+     
+        except:
+            rta = '-1'
 
         return HttpResponse("<h1>Valor correcto</h1><p>" + rta + "</p>")
