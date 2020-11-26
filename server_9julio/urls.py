320358a9c658919e31ad95c7e2a779b0a7d0cef3
ianmajkut <ianedwinmajkut@gmail.com>
2020-11-26 11:45:47 -0300

    drag and drop

--- server_9julio/urls.py
+++ server_9julio/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('general/', include('registroGeneral.urls')),
@@ -28,4 +32,10 @@ urlpatterns = [
     path('', include('usuariosistema.urls')),
     path('usuario/', include('usuario.urls')),
     path('',include('usuario.urls')),
+
+    path('upload/', include('draganddrop.urls')),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
