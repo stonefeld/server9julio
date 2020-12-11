f3d6a81fa510f6a79e62d27debb5bdab276e050c
TheoStanfield <theo.stanfield@gmail.com>
2020-12-11 01:29:26 -0300

    deployment 1

--- server_9julio/urls.py
+++ server_9julio/urls.py
@@ -16,15 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('general/', include('registroGeneral.urls')),
     path('tenis/', include('registroTenis.urls')),
     path('pileta/',include('registroPileta.urls')),
     path('estacionamiento/',include('estacionamiento.urls')),
+
     path('login/', auth_views.LoginView.as_view(template_name='usuariosistema/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='usuariosistema/logout.html'), name='logout'),
 
@@ -32,5 +35,8 @@ urlpatterns = [
     path('usuario/', include('usuario.urls')),
     path('upload/', include('draganddrop.urls')),
 ]
+
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
