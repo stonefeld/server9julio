50c4001c73697967a8d1403020a8d9e18dd41740
TheoStanfield <theo.stanfield@gmail.com>
2020-12-15 19:05:31 -0300

    Update settings.py

--- server_9julio/settings.py
+++ server_9julio/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['192.168.100.39']
 
 
 # Application definition
