6cda82e47f0db14942a907119826aae38d3ac4db
TheoStanfield <theo.stanfield@gmail.com>
2020-12-19 11:24:08 -0300

    client.py

--- scripts/client.py
+++ scripts/client.py
@@ -1,5 +1,4 @@
-import sys
-import socket
+import sys, socket
 
 argv = sys.argv
 argv.remove(argv[0])
@@ -10,7 +9,8 @@ ip = '192.168.100.151'
 
 for i in range(cantidad):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('localhost', 5050)
+    print('Conectando a la ip: " + ip + " en el puerto: 5050')
+    server_address = (ip, 5050)
     s.connect(server_address)
     try:
         if accion == 'abrir_tiempo':
@@ -22,20 +22,20 @@ for i in range(cantidad):
         else:
             message = b'#C'
 
-        print('Sending ' + str(message))
+        print('Enviando ' + str(message))
         s.sendall(message)
 
-        amount_received = 0
-        amount_expected = len(message)
+        #amount_received = 0
+        #amount_expected = len(message)
 
-        while amount_received < amount_expected:
-            data = s.recv(16)
-            amount_received += len(data)
-            print('Received: ' + str(data))
+        #while amount_received < amount_expected:
+        #    data = s.recv(16)
+        #    amount_received += len(data)
+        #    print('Recibido: ' + str(data))
 
-        print('Final data count: ' + str(amount_received))
+        #print('Conteo de datos finales: ' + str(amount_received))
 
     finally:
-        print('Closing socket')
+        print('Cerrando conexión')
         s.close()
 
