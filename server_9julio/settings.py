5be225e0f3c04103bb029ba48144a9bc0e777759
Lucas Affre <lucasaffre@gmail.com>
2021-03-02 16:04:14 -0300

    Intregración Theo

--- server_9julio/settings.py
+++ server_9julio/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': '9Julio_db',
-        'USER': 'admin',
+        'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': 3306,
