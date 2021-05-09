7b3449a9f32691d7747e8e074327b2377dbe12b1
Lucas Affre <lucasaffre@gmail.com>
2021-05-09 13:41:17 -0300

    tarifaHorarios y tarifaEspecial

--- menu_estacionamiento/urls.py
+++ menu_estacionamiento/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('proveedores/', views.proveedores, name='proveedores'),
     path('historial/', historial_estacionamiento, name='historial'),
     path('historialCajas/', views.historial_cajas, name='historialCajas'),
+    path('tarifaEspecial/', views.tarifasEspeciales, name='tarifaEspecial'),
 ]
