e9ab8c5433e556288270883bd6ae5c841c1bfa99
stonefeld - Theo Stanfield <theo.stanfield@gmail.com>
2021-05-16 23:10:49 -0300

    deploy01

--- server_9julio/settings.py
+++ server_9julio/settings.py
@@ -8,7 +8,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os, json
+import os
+import json
 
 import pytz
 from django.utils import timezone
@@ -21,12 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'abcdefghijklmnopqrstuvwxyz1234567890'
+# SECRET_KEY = 'abcdefghijklmnopqrstuvwxyz1234567890'
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
+
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.180', 'server9julio.net', '172.20.1.120']
 
 
 # Application definition
@@ -142,15 +147,6 @@ USE_TZ = False
 timezone.activate(pytz.timezone(TIME_ZONE))
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'usuariosistema:home'
-
-MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
