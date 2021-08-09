2a5b705dc06f897ab3bf6736c7e989c13cf4f665
stonefeld - Theo Stanfield <theo.stanfield@gmail.com>
2021-08-09 17:50:50 -0300

    Arreglo de errores y abrir automaticamente el codigo QR despues del pago o regularizacion de deuda

--- estacionamiento/urls.py
+++ estacionamiento/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('historial/<int:id>/editar/<str:origen>', views.editar_estacionamiento, name='editar'),
     path('historial/<int:id>/detalle/<str:origen>/cobrarEntrada', views.cobrar_entrada, name='cobrarEntrada'),
     path('historial/<int:id>/gen-pdf/<str:origen>', views.generate_pdf, name='gen-pdf'),
+    path('historial/<int:id>/pago_deuda', views.pago_deuda, name='pago_deuda'),
     path('emision-resumen/', views.emision_resumen_mensual, name='resumen-mensual'),
     path('emision-resumen-get/', views.emision_resumen_mensual_get, name='resumen-mensual-get'),
     path('cierre-caja/', views.cierre_caja, name='cierre-caja'),
@@ -18,7 +19,6 @@ urlpatterns = [
     path('proveedor/<int:id>/', views.detalle_proveedor, name='detalle-proveedor'),
     path('proveedor/<int:id>/editar', views.editar_proveedor, name='editar-proveedor'),
     path('proveedor/fetch/', views.fetch_proveedores, name='fetch-proveedores'),
-    path('historial/<int:id>/pago_deuda', views.pago_deuda, name='pago_deuda'),
     path('fetch_Events', views.fetch_events, name='fetch_Events'),
     path('emision_resumen/<int:id>/', views.emision_resumen_anterior, name='emision_anterior')
 ]
