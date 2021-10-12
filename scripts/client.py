7a6bd8b8cc6a1b887a964342b28925c3263f7f1f
Theo Stanfield <theo.stanfield@gmail.com>
2021-10-12 15:06:10 -0300

    Update client.py

--- scripts/client.py
+++ scripts/client.py
@@ -1,4 +1,4 @@
-def client(ip='192.168.0.181', accion='abrir_tiempo', cantidad=1):
+def client(ip='192.168.49.31', accion='abrir_tiempo', cantidad=1):
     import socket
 
     for i in range(cantidad):
