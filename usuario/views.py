d2d4349a6593348bf888f0f46dd559ffeeae6731
stonefeld <theo.stanfield@gmail.com>
2022-06-18 01:13:40 -0300

    Arreglo cambio subida csv para usar col 'total' en vez de 'saldo'

--- usuario/views.py
+++ usuario/views.py
@@ -121,7 +121,7 @@ def cargar_db(request):
         df = pd.read_csv(
             location,
             encoding='latin_1',
-            error_bad_lines=False,
+            on_bad_lines='skip',
             names=list('abcdefghijklmnopqrstuv')
         )
 
@@ -129,21 +129,18 @@ def cargar_db(request):
         messages.warning(request, 'Ha habido un error al leer el archivo')
         return redirect('draganddrop:upload')
 
-    df.drop('b', inplace=True, axis=1)
-    df.drop('d', inplace=True, axis=1)
-
-    for column in list('ghijklmnopqrstuv'):
+    for column in list('bdefghijklmnopqrst'):
         df.drop(str(column), inplace=True, axis=1)
 
     for ind in df.index:
-        if not pd.isna(df['f'][ind]):
-            df['e'][ind] = df['f'][ind]
+        if not pd.isna(df['v'][ind]):
+            df['u'][ind] = df['v'][ind]
 
-    df.drop('f', inplace=True, axis=1)
+    df.drop('v', inplace=True, axis=1)
     df = df.rename(columns={
         'a': 'NrSocio',
         'c': 'Socio',
-        'e': 'Deuda'
+        'u': 'Deuda'
     })
     if df['NrSocio'][5] != 'Composición de Saldos':
         messages.warning(request, 'El archivo subido es incorrecto')
